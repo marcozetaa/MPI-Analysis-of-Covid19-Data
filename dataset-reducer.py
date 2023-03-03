2d982c36fc1a0e87c031275cd3445e107e2c2d25
LucaRondini <luca.rondini@mail.polimi.it>
2023-03-03 14:17:58 +0100

    Added a reduced dataset for testing and the Python script to generate it

---
+++ dataset-reducer.py
@@ -0,0 +1,46 @@
+import csv
+import sys
+
+NUMBER_OF_COUNTRIES = 10
+DAYS_PER_COUNTRY = 20
+
+with open('files/input.csv', newline='') as csvfile:
+    data = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+    output = ""
+    prevCountry = ""
+    next(data) # Skip the first line (header)
+    
+    row = next(data)
+    for i in range(NUMBER_OF_COUNTRIES):
+        while(row[6] == prevCountry): # Skip all the lines until reaching a new country
+            try:
+                row = next(data)
+            except:
+                print("Number of countries is too large")
+                sys.exit()
+        prevCountry = row[6]
+        for j in range(DAYS_PER_COUNTRY):
+            if(prevCountry != row[6]):
+                print("Number of days too large for a country")
+                sys.exit()
+            output += ','.join(row)
+            output += "\n"
+            try:
+                row = next(data)
+            except:
+                print("End of file reached")
+                sys.exit()
+    
+    f = open('files/reduced-dataset.csv', 'w')
+    f.write(output)
+    f.close
+    print("Done.")
+
+    # for row in data:
+    #     if(curDay < DAYS_PER_COUNTRY):
+    #         output += ','.join(row)
+    #         output += "\n"
+    #         curDay = curDay + 1
+    #     elif(curCountry < NUMBER_OF_COUNTRIES):
+    #         curCountry += 1
